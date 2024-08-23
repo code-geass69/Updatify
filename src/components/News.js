04da8ffbd810d44147cecfd7a3b5ebb4cbb9236f
Atharv More <atharvmore30@gmail.com>
2024-08-23 15:37:04 +0530

    modified:   src/components/News.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,52 +1,61 @@
-import React, { useEffect, useState } from 'react'
-
-import NewsItem from './Newsitem'
+import React, { useEffect, useState } from 'react';
+import NewsItem from './Newsitem';
 import Spinner from './Spinner';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const News = (props) => {
-    const [articles, setArticles] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [page, setPage] = useState(1)
-    const [totalResults, setTotalResults] = useState(0)
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [totalResults, setTotalResults] = useState(0);
 
     const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
-    }
+    };
+
+    const sortArticlesByDate = (articles) => {
+        return articles.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
+    };
 
     const updateNews = async () => {
         props.setProgress(10);
         const url = `https://newsapi.org/v2/everything?q=${props.category}&apiKey=${props.apiKey}`;
-        setLoading(true)
+        setLoading(true);
         let data = await fetch(url);
         props.setProgress(30);
-        let parsedData = await data.json()
+        let parsedData = await data.json();
         props.setProgress(70);
-        setArticles(parsedData.articles)
-        setTotalResults(parsedData.totalResults)
-        setLoading(false)
+        // Sort articles by date before setting the state
+        const sortedArticles = sortArticlesByDate(parsedData.articles);
+        setArticles(sortedArticles);
+        setTotalResults(parsedData.totalResults);
+        setLoading(false);
         props.setProgress(100);
-    }
+    };
 
     useEffect(() => {
         document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
         updateNews();
         // eslint-disable-next-line
-    }, [])
+    }, []);
 
     const fetchMoreData = async () => {
-        const url = `https://newsapi.org/v2/everything?q=${props.category}&apiKey=${props.apiKey}`;
-        setPage(page + 1)
+        const url = `https://newsapi.org/v2/everything?q=${props.category}&apiKey=${props.apiKey}&page=${page + 1}`;
+        setPage(page + 1);
         let data = await fetch(url);
-        let parsedData = await data.json()
-        setArticles(articles.concat(parsedData.articles))
-        setTotalResults(parsedData.totalResults)
+        let parsedData = await data.json();
+        // Concatenate and sort articles by date
+        const newArticles = sortArticlesByDate(articles.concat(parsedData.articles));
+        setArticles(newArticles);
+        setTotalResults(parsedData.totalResults);
     };
 
     return (
         <>
-            <h2 className="text-center" style={{ margin: '35px 0px', marginTop: '90px', color: 'whitesmoke' }}> <strong>Updatify - Top <span className='font-bold' style={{color: 'black'}}>{capitalizeFirstLetter(props.category)}</span>  Headlines</strong></h2>
+            <h2 className="text-center" style={{ margin: '35px 0px', marginTop: '90px', color: 'whitesmoke' }}>
+                <strong>Updatify - Top <span className='font-bold' style={{ color: 'black' }}>{capitalizeFirstLetter(props.category)}</span> Headlines</strong>
+            </h2>
             {loading && <Spinner />}
             <InfiniteScroll
                 dataLength={articles.length}
@@ -55,34 +64,37 @@ const News = (props) => {
                 loader={<Spinner />}
             >
                 <div className="container">
-
                     <div className="row">
-                        {articles.map((element) => {
-                            return <div className="col-md-4" key={element.url}>
-                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                        {articles.map((element) => (
+                            <div className="col-md-4" key={element.url}>
+                                <NewsItem
+                                    title={element.title ? element.title : ""}
+                                    description={element.description ? element.description : ""}
+                                    imageUrl={element.urlToImage}
+                                    newsUrl={element.url}
+                                    author={element.author}
+                                    date={element.publishedAt}
+                                    source={element.source.name}
+                                />
                             </div>
-                        })}
+                        ))}
                     </div>
                 </div>
             </InfiniteScroll>
-            
-
         </>
-    )
-
-}
-
+    );
+};
 
 News.defaultProps = {
     country: 'in',
     pageSize: 8,
     category: 'general',
-}
+};
 
 News.propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string,
-}
+};
 
-export default News
+export default News;
