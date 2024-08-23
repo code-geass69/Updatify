f228e195a38215a3fa9c2192ba5e280fd9757e80
Atharv More <atharvmore30@gmail.com>
2024-08-23 15:35:46 +0530

    modified:   src/App.css 	modified:   src/components/Navbar.js 	modified:   src/components/News.js 	new file:   src/styles/Navbar.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import '../styles/Navbar.css';
 
 const NavBar = () => {
+
+    const [isAtTop, setIsAtTop] = useState(true);
+
+    const handleScroll = () => {
+        if (window.scrollY === 0) {
+            setIsAtTop(true);
+        } else {
+            setIsAtTop(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div>
-            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar fixed-top navbar-expand-lg">
                 <div className="container-fluid">
-                    <Link className="navbar-brand mx-3" to="/">Updatify</Link>
+                    <Link className="navbar-brand mx-3" style={{color: 'blue', paddingLeft:'20px'}} to="/"> <strong>Updatify</strong></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse "  id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 gap-lg-5" style={{ marginLeft: '200px' }}>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 gap-lg-5" style={{ marginLeft: '150px' }}>
                             <li className="nav-item">
                                 <Link className="nav-link" to="/business">Business</Link>
                             </li>
@@ -28,10 +51,13 @@ const NavBar = () => {
                                 <Link className="nav-link" to="/science">Science</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/sports">Sports</Link>
+                                <Link className="nav-link  " to="/sports">Sports</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/technology">Technology</Link>
+                                <Link className="nav-link  " to="/technology">Technology</Link>
+                            </li>
+                            <li className="nav-item">
+                                <button type="button" className="btn btn-light btn-floating btn-sm mx-5 my-1" onClick={scrollToTop} disabled={isAtTop}>Go to Top</button>
                             </li>
                         </ul>
                     </div>
--- src/components/News.js
+++ src/components/News.js
@@ -1,23 +1,23 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 import NewsItem from './Newsitem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const News = (props)=>{
+const News = (props) => {
     const [articles, setArticles] = useState([])
     const [loading, setLoading] = useState(true)
     const [page, setPage] = useState(1)
     const [totalResults, setTotalResults] = useState(0)
-    
+
     const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
-    } 
+    }
 
-    const updateNews = async ()=> {
+    const updateNews = async () => {
         props.setProgress(10);
-        const url = `https://newsapi.org/v2/everything?q=${props.category}&apiKey=${props.apiKey}`; 
+        const url = `https://newsapi.org/v2/everything?q=${props.category}&apiKey=${props.apiKey}`;
         setLoading(true)
         let data = await fetch(url);
         props.setProgress(30);
@@ -31,32 +31,31 @@ const News = (props)=>{
 
     useEffect(() => {
         document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
-        updateNews(); 
+        updateNews();
         // eslint-disable-next-line
     }, [])
 
-// https://newsapi.org/v2/everything?q=${props.category}&apiKey=${props.apiKey}
-    const fetchMoreData = async () => {   
+    const fetchMoreData = async () => {
         const url = `https://newsapi.org/v2/everything?q=${props.category}&apiKey=${props.apiKey}`;
-        setPage(page+1) 
+        setPage(page + 1)
         let data = await fetch(url);
         let parsedData = await data.json()
         setArticles(articles.concat(parsedData.articles))
         setTotalResults(parsedData.totalResults)
-      };
- 
-        return (
-            <>
-                <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px', color: 'wheat' }}>Updatify - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
-                {loading && <Spinner />}
-                <InfiniteScroll
-                    dataLength={articles.length}
-                    next={fetchMoreData}
-                    hasMore={articles.length !== totalResults}
-                    loader={<Spinner/>}
-                > 
-                    <div className="container">
-                         
+    };
+
+    return (
+        <>
+            <h2 className="text-center" style={{ margin: '35px 0px', marginTop: '90px', color: 'whitesmoke' }}> <strong>Updatify - Top <span className='font-bold' style={{color: 'black'}}>{capitalizeFirstLetter(props.category)}</span>  Headlines</strong></h2>
+            {loading && <Spinner />}
+            <InfiniteScroll
+                dataLength={articles.length}
+                next={fetchMoreData}
+                hasMore={articles.length !== totalResults}
+                loader={<Spinner />}
+            >
+                <div className="container">
+
                     <div className="row">
                         {articles.map((element) => {
                             return <div className="col-md-4" key={element.url}>
@@ -64,11 +63,13 @@ const News = (props)=>{
                             </div>
                         })}
                     </div>
-                    </div> 
-                </InfiniteScroll>
-            </>
-        )
-    
+                </div>
+            </InfiniteScroll>
+            
+
+        </>
+    )
+
 }
 
 
